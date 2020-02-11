bfea536f7b48a02156dbb9f19734b5f51d1e72d9
fabiansm5 <43194173+fabiansm5@users.noreply.github.com>
2020-02-11 13:06:28 -0500

    Int
    Made tab

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+function openTabIndex(evt, tabIndex)
+{
+    // Declare all variables
+    var i, tablinks;
+   
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabIndex).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+//Open dafault tab index
+document.getElementById("defaultOpen").click();
