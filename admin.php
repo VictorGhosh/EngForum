02cf1b924bcf4e1711c420be7059eb47feccd6c3
Victor <36003172+VictorGhosh@users.noreply.github.com>
2020-02-26 19:43:50 -0500

    Closer to pushing data to SQL from PHP
    Data is now encrypted and can be sent to server. Server is not accepting data as it is not falling on the right table.

--- admin.php
+++ admin.php
@@ -2,34 +2,31 @@
 <!-- DO NOT EVER LINK TO THIS PAGE -->
 
 <?php
+    //ID for access to edit Client dataTables
     $dbhost = 'localhost';
     $dbuser = 'clientMaster';
     $dbpass = 'Password';
     $db = 'CLIENTUSERDATA';
 
+    //Connect to SQL ($link)
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
-
     if(! $conn ) {
-        die('Could not connect: ');
+        die('Could not connect: ' . mysqli_error($conn));
     }
 
-    if(! get_magic_quotes_gpc() ) {
-        $username = addslashes ('Test Username');
-        $password = addslashes ('Test Password');
-    } else {
-        $username = 'Test Username (no slash)';
-        $password = 'Test Password (no slash)';
-    }
-    $creation_date = 'Test Creation Date';
-    $sql = "INSERT INTO clientLogin ".
-        "(username,password,creation_date) ".
-        "VALUES"."($username, $password, $creation_date)";
+    //Data to input
+    $user_id = '2';
+    $username = mysqli_real_escape_string($conn, 'JaneDoe');
+    $password = mysqli_real_escape_string($conn, 'JanesPassword');
+    $creation_date = mysqli_real_escape_string($conn,'1/22/2456');
+
+    $sqli = "INSERT into clientLogin VALUES ($user_id, $username, $password, $creation_date)";
 
     mysqli_select_db($conn, $db);
-    $retval = mysqli_query( $sql, $conn );
+    $retval = mysqli_real_query($conn, $sqli);
       
     if(! $retval ) {
-    die('Could not enter data: ');
+    die('Could not enter data: ' . mysqli_error($conn));
     }
     echo "Entered data successfully\n";
 
