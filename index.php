3126af586794d87f397f9bb0318ae7d78371de25
Victor <36003172+VictorGhosh@users.noreply.github.com>
2020-03-10 18:05:28 -0400

    Changed alot
    Giving up on getting Ajax to work for now because we are super low on time.  Going to do all backend in one file. stupud idea but will be easier. right now it is broken and I have no idea why.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 
-<h1 style="color:#990000;text-align:left;font-size:60px"><strong> Welcome to Queens Applied Science Forum!</strong></h1>
+<h1 style="color:#990000;text-align:left;font-size:60px"><strong> Welcome to Queen's Applied Science Forum!</strong></h1>
 
 <!--This is the tab links-->
 <div class="tab">
@@ -34,21 +34,20 @@
 
 <!--Form submission of the user's data-->
 <div id='createUser' class='tabcontent'>
-    <?php
-        // include 'giveUser.php';
-        //     if ( ! giveUser.php)
-        // if( isset($_POST['submit']) )
-        // {
-        //     //TODO: Fix this
-        //     $val1 = htmlentities($_POST['uname']);
-        //     $val2 = htmlentities($_POST['upass']);
-        // }
-    ?>
-    <form action="giveUser.php" method="post">
-        <input type="text" placeholder="Username" name="uname">
-        <input type="text" placeholder="Password" name="upass">
-        <input type="submit">
-    </form>
+<!-- 
+    <script>
+        $(document).ready(function() {
+            $(bSubmitCreate).click(function() {
+                // $_SESSION["iUName"] = document.getElementById("createUName").value;
+                // $_SESSION["iUPass"] = document.getElementById("createUPass").value;
+            
+                // giveUser($_POST["iUName"],$_POST["iUPass"]);
+            });
+        });
+    </script>
+        <input type="text" placeholder="Username" id="createUName">
+        <input type="text" placeholder="Password" id="createUPass">
+        <button id="bSubmitCreate">Submit</button> -->
 </div>
 
 <!-- Login -->
@@ -61,14 +60,17 @@
 
                 //Save user input as variables
                 var iUName = document.getElementById("loginUNameID").value;
-                var iUPass = document.getElementById("loginUNameID").value;
-                alert(iUName);
+                var iUPass = document.getElementById("loginUPassID").value;
+
+                //Ditched ajax. can't figure it out
+                //alert(iUPassPage);
                 // $.ajax({
                 //     type: "POST",
-                //     url: "getUser.php",
-                //     data: { iUser: iUsername }
-                // }).done(function( msg ) {
-                //     alert( "Input sent to login checker: " + msg );
+                //     url: "test.php",
+                //     data: { uPass: iUPass},
+                //     success: function(result){
+                //          alert('success');
+                //     }
                 // });
             });
         });
@@ -92,8 +94,92 @@
     <!--Display input in the tabs-->
 </div>
 
-    <script src="index.js">
-    </script>
+<?php  /*
+        * Putting php methods here because I cannot for the life of me
+        * get $_POST to work with Query. WHY IN THE WORLD DO I NEED TO
+        * USE 3 DIFFERNT LANGUAGES!
+        */
+    
+    // function giveUser($uName, $uPass) {
+
+    //     //Begin static vars
+    //     $db = 'CLIENTUSERDATA';
+    //     $conn = dbConnect();
+    //     $username = mysqli_real_escape_string($conn, $_POST["uname"]);
+    //     $password = mysqli_real_escape_string($conn, $_POST["upass"]);
+    //     //End static vars
+
+    //     //Create Table entry
+    //     $sqli = "INSERT INTO clientLogin VALUES ('$username', '$password')";
+    //     mysqli_select_db($conn, $db);
+    //     $retval = mysqli_real_query($conn, $sqli);
+      
+    //     if (! $retval ) {
+    //     die('Could not enter data: ' . mysqli_error($conn));
+    //     }
+
+    //     header("Location: http://quasf.engineering.queensu.ca/createProfile.php");
+    //     mysqli_close($conn);
+    // }
+    
+    // function userPassCheck($iUser,$iPass) {
+
+    //     //Begin static vars
+    //     $db = 'CLIENTUSERDATA';
+    //     $conn = dbConnect();
+        
+    //     //Get user input from html page and "encrypt"
+    //     $username = mysqli_real_escape_string($conn, $iUser);
+    //     $password = mysqli_real_escape_string($conn, $iPass);
+    //     //End static vars
+
+    //     //Begin nonstatic vars
+    //     $currentUser = null;
+    //     //End nonstatic vars
+
+    //     //Get list of current usernames from database
+    //     $sql = "SELECT uname, upass FROM clientLogin";
+    //     mysqli_select_db($conn, $db);
+    //     $result = mysqli_query($conn, $sql);
+    //     if(! $result) {
+    //         die('Could not retrive username data: ' . mysqli_error($conn));
+    //     }
+
+    //     //Run through all usernames because I am lazy
+    //     while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    //         //Check if username matches input
+    //         if ($row["uname"] === $username) {
+    //             $currentUser = $row["uname"];
+    //             //Check if password matches input
+    //             if ($row["upass"] === $password) {
+    //                 //User accepted
+    //                 userLoginYes($username);
+    //             } else {
+    //                 //Login fail due to nonexistent username
+    //             }
+    //             return;
+    //         } else {
+    //             $currentUser = null;
+    //         }
+    //     } if ($currentUser == null) {
+    //         //Login fail due to nonexistent password
+    //     }
+    //     mysqli_close($conn);
+    // }
+
+    // function dbConnect() {
+    //     //ID for access to edit Client dataTables
+    //     $dbhost = 'localhost';
+    //     $dbuser = 'clientMaster';
+    //     $dbpass = 'Password';
+    //     //Connect to SQL ($link)
+    //     $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+    //     if(! $conn ) {
+    //         die('Could not connect: ' . mysqli_error($conn));
+    //     }
+    //     return $conn;
+    // }
+?>
 
 </body>
 </html>
